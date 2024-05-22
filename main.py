5f0d032692d056aa051000bf407617a87924b9c9
Artem Zinukov <zinykov_a@mail.ru>
2024-05-22 20:52:35 +0300

    Создание проекта

--- main.py
+++ main.py
@@ -1,41 +1,27 @@
-import pprint
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 import datetime
-import pandas
 import json
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 from collections import defaultdict
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+import pandas
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-template = env.get_template('template.html')
-creation_date = 1920
-current_date = datetime.date.today().year
-delta_time = current_date - creation_date
-wines = (pandas.read_excel('wine3.xlsx', sheet_name="Лист1",
+
+def load_wine_data():
+    return (pandas.read_excel('wine3.xlsx', sheet_name="Лист1",
                            usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'],
                            engine='openpyxl')
-         .to_json(orient="records", force_ascii=False))
-
-wines = json.loads(wines)
-wine_list = defaultdict(list)
-for wine in wines:
-    wine_category = wine["Категория"]
-    wine_list[wine_category].append(wine)
-
-wine_parameters = None
-for wine, sad in wine_list.items():
-    print(wine)
-    print(sad)
-    wine_parameters = wine_list[wine]
-    # for asad in wine_parameters:
-        # print(asad)
+            .to_json(orient="records", force_ascii=False))
 
 
-def year_format(year):
+def calculate_time_foundation():
+    creation_date = 1920
+    current_date = datetime.date.today().year
+    delta_time = current_date - creation_date
+    return delta_time
+
+
+def get_year_format(year):
     year = year % 100
     if 21 > year > 4:
         return "лет"
@@ -47,16 +33,32 @@ def year_format(year):
     return "лет"
 
 
-rendered_page = template.render(
-    date=delta_time,
-    year=year_format(delta_time),
-    wines=wines,
-    wine_list=wine_list,
-    wine_parameters=wine_parameters
-)
+def render_template(wines, wine_list, delta_time):
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+    return template.render(date=delta_time,
+                           year=get_year_format(delta_time),
+                           wines=wines,
+                           wine_list=wine_list)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def main():
+    wines = json.loads(load_wine_data())
+    wine_list = defaultdict(list)
+    for wine in wines:
+        wine_category = wine["Категория"]
+        wine_list[wine_category].append(wine)
+
+    rendered_page = render_template(wines, wine_list, calculate_time_foundation())
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
